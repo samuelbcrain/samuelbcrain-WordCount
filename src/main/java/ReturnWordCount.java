d1d9217121396149e2cf99b231e80b8a2616ce5c
Samuel Crain <samuelbcrain@gmail.com>
2025-02-07 07:17:17 +0000

    Coded method to find amount of words in a given string.

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -5,6 +5,6 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        return in.split(" ").length;
     }
 }
